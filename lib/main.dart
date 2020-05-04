e76faa7a2c75075bd5f8a825f6d8e018719041f4
Jan Puig <janpuigcapella@gmail.com>
2020-05-04 08:52:52 +0200

    añadir photo View /photo gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:photo_view/photo_view.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 void main() => runApp(MyApp());
 
@@ -38,7 +40,7 @@ class ImageGallery extends StatelessWidget {
                 onTap: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (context) => ImagePage(docs[index].data['url']),
+                      builder: (context) => PhotoGallery(docs, index),
                     ),
                   );
                 },
@@ -68,14 +70,59 @@ class ImagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-        ),
+      appBar: AppBar(
         backgroundColor: Colors.black,
-        body: Center(
-          child: Image.network(
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: PhotoView(
+          imageProvider: NetworkImage(
             url,
           ),
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class PhotoGallery extends StatelessWidget {
+  final galleryItems;
+  final int pos;
+
+  PhotoGallery(this.galleryItems, this.pos);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+          child: PhotoViewGallery.builder(
+        scrollPhysics: const BouncingScrollPhysics(),
+        builder: (BuildContext context, int index) {
+          index=pos;
+          return PhotoViewGalleryPageOptions(
+            imageProvider: NetworkImage(
+              galleryItems[index].data['url'],
+            ),
+            initialScale: PhotoViewComputedScale.contained,
+          );
+        },
+        itemCount: galleryItems.length,
+        loadingBuilder: (context, event) => Center(
+          child: Container(
+            width: 20.0,
+            height: 20.0,
+            child: CircularProgressIndicator(
+              value: event == null
+                  ? 0
+                  : event.cumulativeBytesLoaded / event.expectedTotalBytes,
+            ),
+          ),
+        ),
+      )),
+    );
   }
 }
